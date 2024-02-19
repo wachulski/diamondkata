2bd86f6c42b330103f4cf331e7f92ef37dd5b8e0
Marcin Wachulski <wachulski@users.noreply.github.com>
2024-02-19 10:44:04 +0100

    🟢 Implement ABBA diamond

--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -20,14 +20,21 @@ public class Diamond
 
         var diamondBuilder = new StringBuilder();
 
-        for (var i = 0; i <= _alphaDistance; i++)
+        var increment = 1;
+        for (var i = 0; i >= 0; i+=increment)
         {
             var currentDiamondLetter = (char)('A' + i);
             var spacesAround = new string(' ', _alphaDistance - i);
             var diamondPart = i == 0
                 ? currentDiamondLetter.ToString()
                 : $"{currentDiamondLetter}{new string(' ', i * 2 - 1)}{currentDiamondLetter}";
-            diamondBuilder.Append(spacesAround).Append(diamondPart).Append(spacesAround).AppendLine();
+            diamondBuilder.Append(spacesAround).Append(diamondPart).Append(spacesAround);
+            
+            if (i > 0 || increment > 0)
+                diamondBuilder.AppendLine();
+            
+            if (i == _alphaDistance)
+                increment = -1;
         }
 
         return diamondBuilder.ToString();
