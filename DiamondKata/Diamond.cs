9c677600a38b05c5d9ab8becd3e8f3b7ec52e003
Marcin Wachulski <wachulski@users.noreply.github.com>
2024-02-19 11:02:56 +0100

    🟢 Accept lowercase chars

--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -9,10 +9,10 @@ public class Diamond
 
     public Diamond(char letter)
     {
-        if (letter is < 'A' or > 'Z')
+        if (char.ToUpper(letter) is < 'A' or > 'Z')
             throw new ArgumentException("Letter must be a letter from A to Z");
         
-        _letter = letter;
+        _letter = char.ToUpper(letter);
         _alphaDistance = _letter - 'A';
     }
 
