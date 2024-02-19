a36de1f0dca6da8df27bc3420d8adf8b3dd1d370
Marcin Wachulski <wachulski@users.noreply.github.com>
2024-02-19 09:45:44 +0100

    🔴 Test for 'B' case - many parts diamond

--- DiamondKata.UnitTests/DiamondTests.cs
+++ DiamondKata.UnitTests/DiamondTests.cs
@@ -3,12 +3,25 @@ namespace DiamondKata.UnitTests;
 public class DiamondTests
 {
     [Fact]
-    public void When_just_letter_A_Then_Should_Print_Just_A()
+    public void When_just_letter_A_Then_should_print_just_A()
     {
         var diamond = new Diamond('A');
 
         var actual = diamond.ToString();
 
         Assert.Equal("A", actual);
+    }
+
+    [Fact]
+    public void When_B_ie_many_parts_Then_should_print_ABBA_diamond()
+    {
+        var diamond = new Diamond('B');
+        var expected = @"A
+B B
+ A";
+
+        var actual = diamond.ToString();
+
+        Assert.Equal(expected, actual);
     }
 }
