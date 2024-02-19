a8b262b62c636adee9f92ed52b8910fcd6ee412c
Marcin Wachulski <wachulski@users.noreply.github.com>
2024-02-19 09:36:44 +0100

    🟢 Implement 'A' case

--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -2,8 +2,15 @@ namespace DiamondKata;
 
 public class Diamond
 {
+    private readonly char _letter;
+
     public Diamond(char letter)
     {
-        
+        _letter = letter;
+    }
+    
+    public override string ToString()
+    {
+        return _letter.ToString();
     }
 }
