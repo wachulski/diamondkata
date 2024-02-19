8e955441628945b66d55b980de6a81eb371288f6
Marcin Wachulski <wachulski@users.noreply.github.com>
2024-02-19 10:27:01 +0100

    🛠️ Refactor local names in diamond implementation

--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -6,28 +6,30 @@ public class Diamond
 {
     private readonly char _letter;
     private readonly int _alphaDistance;
-        
+
     public Diamond(char letter)
     {
         _letter = letter;
         _alphaDistance = _letter - 'A';
     }
-    
+
     public override string ToString()
     {
         if (_alphaDistance == 0)
             return _letter.ToString();
 
         var diamondBuilder = new StringBuilder();
-        
+
         for (var i = 0; i <= _alphaDistance; i++)
         {
-            var letter = (char)('A' + i);
-            var spaces = new string(' ', _alphaDistance - i);
-            var diamondPart = i == 0 ? letter.ToString() : $"{letter}{new string(' ', i * 2 - 1)}{letter}";
-            diamondBuilder.Append(spaces).Append(diamondPart).Append(spaces).AppendLine();
+            var currentDiamondLetter = (char)('A' + i);
+            var spacesAround = new string(' ', _alphaDistance - i);
+            var diamondPart = i == 0
+                ? currentDiamondLetter.ToString()
+                : $"{currentDiamondLetter}{new string(' ', i * 2 - 1)}{currentDiamondLetter}";
+            diamondBuilder.Append(spacesAround).Append(diamondPart).Append(spacesAround).AppendLine();
         }
-        
+
         return diamondBuilder.ToString();
     }
 }
