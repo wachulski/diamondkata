1107d0b96fff246d44edea2bac30e9330b523e9e
Marcin Wachulski <wachulski@users.noreply.github.com>
2024-02-19 11:12:09 +0100

    🛠️ Improve readability of the loop variables

--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -5,7 +5,7 @@ namespace DiamondKata;
 public class Diamond
 {
     private readonly char _letter;
-    private readonly int _alphaDistance;
+    private readonly int _midpointDistance;
 
     public Diamond(char letter)
     {
@@ -13,31 +13,31 @@ public class Diamond
             throw new ArgumentException("Letter must be a letter from A to Z");
         
         _letter = char.ToUpper(letter);
-        _alphaDistance = _letter - 'A';
+        _midpointDistance = _letter - 'A';
     }
 
     public override string ToString()
     {
-        if (_alphaDistance == 0)
+        if (_midpointDistance == 0)
             return _letter.ToString();
 
         var diamondBuilder = new StringBuilder();
 
-        var i = 0;
+        var currentDistance = 0;
         var increment = 1;
-        while (i >= 0)
+        while (currentDistance >= 0)
         {
-            var currentDiamondLetter = (char)('A' + i);
-            var spacesAround = new string(' ', _alphaDistance - i);
-            var diamondPart = i == 0
+            var currentDiamondLetter = (char)('A' + currentDistance);
+            var spacesAround = new string(' ', _midpointDistance - currentDistance);
+            var diamondPart = currentDistance == 0
                 ? currentDiamondLetter.ToString()
-                : $"{currentDiamondLetter}{new string(' ', i * 2 - 1)}{currentDiamondLetter}";
+                : $"{currentDiamondLetter}{new string(' ', currentDistance * 2 - 1)}{currentDiamondLetter}";
             diamondBuilder.Append(spacesAround).Append(diamondPart).Append(spacesAround).AppendLine();
             
-            if (i == _alphaDistance)
+            if (currentDistance == _midpointDistance)
                 increment = -1;
 
-            i += increment;
+            currentDistance += increment;
         }
 
         return diamondBuilder.ToString().TrimEnd('\n').TrimEnd('\r');
