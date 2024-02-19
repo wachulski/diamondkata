a14afb36e9df4b7f31a2388b4d0d1b06746a2b48
Marcin Wachulski <wachulski@users.noreply.github.com>
2024-02-19 10:07:16 +0100

    🛠️ Refactor test names

--- DiamondKata.UnitTests/DiamondTests.cs
+++ DiamondKata.UnitTests/DiamondTests.cs
@@ -3,7 +3,7 @@ namespace DiamondKata.UnitTests;
 public class DiamondTests
 {
     [Fact]
-    public void When_just_letter_A_Then_should_print_just_A()
+    public void When_A_Then_should_print_just_A()
     {
         var diamond = new Diamond('A');
 
@@ -24,7 +24,7 @@ public class DiamondTests
     }
 
     [Fact]
-    public void When_B_ie_many_parts_Then_should_print_ABBA_diamond()
+    public void When_B_Then_should_print_ABBA_diamond()
     {
         var diamond = new Diamond('B');
         var expected = @"A
