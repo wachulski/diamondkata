a49133d9b78d6eb44edd0288ffa442ec966b4b6f
Marcin Wachulski <wachulski@users.noreply.github.com>
2024-02-19 10:04:27 +0100

    🟢 Add 'top line' implementation

--- DiamondKata/Diamond.cs
+++ DiamondKata/Diamond.cs
@@ -3,14 +3,21 @@ namespace DiamondKata;
 public class Diamond
 {
     private readonly char _letter;
-
+    private readonly int _alphaDistance;
+        
     public Diamond(char letter)
     {
         _letter = letter;
+        _alphaDistance = _letter - 'A';
     }
     
     public override string ToString()
     {
-        return _letter.ToString();
+        if (_alphaDistance == 0)
+            return _letter.ToString();
+        
+        var spaces = new string(' ', _alphaDistance);
+        
+        return $"{spaces}A{spaces}";
     }
 }
